b5fe37b2f0dd6c840789ecd608209be9df5dd620
mikkelbrevsbech-star <mikkel.b.revsbech@gmail.com>
2026-03-16 18:03:27 +0100

    i make the game better

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ class SpriteKind:
 def on_b_pressed():
     global top, mySprite, projectile
     sprites.destroy(mySprite3)
+    music.play(music.create_sound_effect(WaveShape.SQUARE,
+            1,
+            4260,
+            0,
+            255,
+            1000,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
     top = sprites.create(img("""
             2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
             . . . . . . . . . . . . . . . .
@@ -172,7 +181,6 @@ def on_b_pressed():
         SpriteKind.player)
     mySprite.set_position(79, 104)
     controller.move_sprite(mySprite, 150, 0)
-    info.start_countdown(300)
     for index in range(500):
         pause(200)
         top.set_position(randint(0, 155), 1)
@@ -223,7 +231,7 @@ def on_b_pressed():
             randint(-30, 30),
             100)
     pause(1000)
-    for index3 in range(2000):
+    for index3 in range(20000000000):
         pause(50)
         top.set_position(randint(0, 155), 1)
         projectile = sprites.create_projectile_from_sprite(img("""
@@ -247,13 +255,20 @@ def on_b_pressed():
             top,
             randint(-40, 40),
             100)
-    game.game_over(True)
-    game.set_game_over_effect(True, effects.confetti)
 controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
 
 def on_a_pressed():
     global top, mySprite, projectile
     sprites.destroy(mySprite3)
+    music.play(music.create_sound_effect(WaveShape.SQUARE,
+            1,
+            4260,
+            0,
+            255,
+            1000,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
     top = sprites.create(img("""
             2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
             . . . . . . . . . . . . . . . .
@@ -420,7 +435,6 @@ def on_a_pressed():
         SpriteKind.player)
     mySprite.set_position(79, 104)
     controller.move_sprite(mySprite, 150, 0)
-    info.start_countdown(300)
     for index4 in range(500):
         pause(200)
         top.set_position(randint(0, 155), 1)
@@ -471,7 +485,7 @@ def on_a_pressed():
             randint(-30, 30),
             100)
     pause(1000)
-    for index6 in range(2000):
+    for index6 in range(20000000):
         pause(50)
         top.set_position(randint(0, 155), 1)
         projectile = sprites.create_projectile_from_sprite(img("""
@@ -495,21 +509,28 @@ def on_a_pressed():
             top,
             randint(-40, 40),
             100)
-    game.game_over(True)
-    game.set_game_over_effect(True, effects.confetti)
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
 def on_on_overlap(sprite, otherSprite):
     info.change_life_by(-1)
+    music.play(music.create_sound_effect(WaveShape.SINE,
+            5000,
+            0,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
     pause(1000)
 sprites.on_overlap(SpriteKind.projectile, SpriteKind.player, on_on_overlap)
 
-def on_countdown_end():
-    pass
-info.on_countdown_end(on_countdown_end)
-
 def on_on_overlap2(sprite2, otherSprite2):
     info.change_life_by(1)
+    music.play(music.create_song(hex("""
+            0078000408010300001c00010a006400f4016400000400000000000000000000000000050000042c000000040001250400080002242508000c0001200c001000012410001400012718001c0001241c002000021d2705001c000f0a006400f4010a00000400000000000000000000000000000000020c0008000c00012914001800011e09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c80019000400050002020b0c000d00010110001100010418001900010a
+            """)),
+        music.PlaybackMode.UNTIL_DONE)
     sprites.destroy(liv2, effects.hearts, 1000)
 sprites.on_overlap(SpriteKind.player, SpriteKind.liv, on_on_overlap2)
 
@@ -565,63 +586,93 @@ def on_a_released():
     liv2.set_velocity(0, 100)
 controller.A.on_event(ControllerButtonEvent.RELEASED, on_a_released)
 
+def on_life_zero():
+    global current_score, leaderboard_text
+    top_scores = settings.read_number_array("leaderboard")
+    if top_scores == None:
+        top_scores = []
+    # 2. Tilføj din nuværende score
+    current_score = info.score()
+    top_scores.append(current_score)
+    top_scores.sort()
+    top_scores.reverse()
+    # 4. Behold kun de 5 bedste
+    if len(top_scores) > 5:
+        top_scores = top_scores.slice(0, 5)
+    settings.write_number_array("leaderboard", top_scores)
+    # 6. Lav teksten manuelt med en løkke (meget mere stabilt)
+    leaderboard_text = "TOP 5 SCORES:"
+    for score_val in top_scores:
+        leaderboard_text = "" + leaderboard_text + "\n" + ("" + str(score_val))
+    # 7. Vis og slut
+    game.show_long_text(leaderboard_text, DialogLayout.CENTER)
+    game.game_over(False)
+info.on_life_zero(on_life_zero)
+
 def on_b_released():
     global mySprite2, liv2
-    for index8 in range(10):
-        pause(randint(1000, 5000))
-        mySprite2 = sprites.create(img("""
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . 9 9 9 9 9 9 9 . . . . .
-                . . . . 9 9 9 9 9 9 9 . . . . .
-                . . . . 9 9 8 8 8 9 9 . . . . .
-                . . . . 9 9 8 5 8 9 9 . . . . .
-                . . . . 9 9 8 8 8 9 9 . . . . .
-                . . . . 9 9 9 9 9 9 9 . . . . .
-                . . . . 9 9 9 9 9 9 9 . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
-                . . . . . . . . . . . . . . . .
+    for index8 in range(100000):
+        for index9 in range(10):
+            pause(randint(1000, 5000))
+            mySprite2 = sprites.create(img("""
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . 9 9 9 9 9 9 9 . . . . .
+                    . . . . 9 9 9 9 9 9 9 . . . . .
+                    . . . . 9 9 8 8 8 9 9 . . . . .
+                    . . . . 9 9 8 5 8 9 9 . . . . .
+                    . . . . 9 9 8 8 8 9 9 . . . . .
+                    . . . . 9 9 9 9 9 9 9 . . . . .
+                    . . . . 9 9 9 9 9 9 9 . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    . . . . . . . . . . . . . . . .
+                    """),
+                SpriteKind.food)
+            mySprite2.set_position(randint(0, 150), 0)
+            mySprite2.set_velocity(0, 100)
+        liv2 = sprites.create(img("""
+                ....................
+                ....................
+                ....................
+                ....2222...2222.....
+                ...222222.222222....
+                ..222212222222222...
+                ..222122222222222...
+                ..221222222222222...
+                ..222222222222222...
+                ..222222222222222...
+                ..222222222222222...
+                ...2222222222222....
+                ....22222222222.....
+                .....222222222......
+                ......2222222.......
+                .......22222........
+                ........222.........
+                .........2..........
+                .........2..........
+                ....................
                 """),
-            SpriteKind.food)
-        mySprite2.set_position(randint(0, 150), 0)
-        mySprite2.set_velocity(0, 100)
-    liv2 = sprites.create(img("""
-            ....................
-            ....................
-            ....................
-            ....2222...2222.....
-            ...222222.222222....
-            ..222212222222222...
-            ..222122222222222...
-            ..221222222222222...
-            ..222222222222222...
-            ..222222222222222...
-            ..222222222222222...
-            ...2222222222222....
-            ....22222222222.....
-            .....222222222......
-            ......2222222.......
-            .......22222........
-            ........222.........
-            .........2..........
-            .........2..........
-            ....................
-            """),
-        SpriteKind.liv)
-    liv2.set_position(randint(0, 150), 0)
-    liv2.set_velocity(0, 100)
+            SpriteKind.liv)
+        liv2.set_position(randint(0, 150), 0)
+        liv2.set_velocity(0, 100)
 controller.B.on_event(ControllerButtonEvent.RELEASED, on_b_released)
 
 def on_on_overlap3(sprite3, otherSprite3):
     info.change_score_by(100)
     sprites.destroy(mySprite2, effects.confetti, 500)
+    music.play(music.create_song(hex("""
+            0078000408010205001c000f0a006400f4010a0000040000000000000000000000000000000002180000000800012708001000012710001800012718002000012708001c000e050046006603320000040a002d0000006400140001320002010002180000000800012008001000011d10001800012018002000011b
+            """)),
+        music.PlaybackMode.UNTIL_DONE)
 sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap3)
 
+leaderboard_text = ""
+current_score = 0
 mySprite2: Sprite = None
 liv2: Sprite = None
 projectile: Sprite = None
@@ -648,3 +699,24 @@ mySprite3 = sprites.create(img("""
         """),
     SpriteKind.wizard)
 mySprite3.say_text("press a to easy press b to hard", 200000, False)
+
+# Sæt volumen lidt ned i starten, så det ikke brager
+music.set_volume(80)
+
+def on_forever():
+    # 1. En dyb "rum-drone" (lav tone)
+    music.play_tone(131, music.beat(BeatFraction.WHOLE))
+    
+    # 2. En lille pause for at skabe spænding
+    pause(200)
+    
+    # 3. Et tilfældigt "stjerneskud" (høj tone)
+    # Vi vælger en tilfældig lys tone mellem 600 og 900 Hz
+    stjerne_lyd = randint(600, 900)
+    music.play_tone(stjerne_lyd, music.beat(BeatFraction.SIXTEENTH))
+    
+    # 4. Lang pause før loopet starter forfra
+    pause(2000)
+
+forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ namespace SpriteKind {
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     sprites.destroy(mySprite3)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 4260, 0, 255, 1000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     top = sprites.create(img`
         2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
         . . . . . . . . . . . . . . . . 
@@ -166,7 +167,6 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         `, SpriteKind.Player)
     mySprite.setPosition(79, 104)
     controller.moveSprite(mySprite, 150, 0)
-    info.startCountdown(300)
     for (let index = 0; index < 500; index++) {
         pause(200)
         top.setPosition(randint(0, 155), 1)
@@ -210,10 +210,10 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            `, top, randint(-30, 30), 100)
+            `, top, randint(-30, 30), 150)
     }
     pause(1000)
-    for (let index = 0; index < 2000; index++) {
+    for (let index = 0; index < 20000; index++) {
         pause(50)
         top.setPosition(randint(0, 155), 1)
         projectile = sprites.createProjectileFromSprite(img`
@@ -233,13 +233,57 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            `, top, randint(-40, 40), 100)
+            `, top, randint(-40, 40), 200)
+    }
+    for (let index = 0; index < 20000000; index++) {
+        pause(10)
+        top.setPosition(randint(0, 155), 1)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, top, randint(-40, 50), 100)
+    }
+    for (let index = 0; index < 20000000; index++) {
+        pause(1)
+        top.setPosition(randint(0, 155), 1)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, top, randint(-50, 50), 100)
     }
     game.gameOver(true)
-    game.setGameOverEffect(true, effects.confetti)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     sprites.destroy(mySprite3)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 4260, 0, 255, 1000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     top = sprites.create(img`
         2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
         . . . . . . . . . . . . . . . . 
@@ -402,8 +446,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         `, SpriteKind.Player)
     mySprite.setPosition(79, 104)
     controller.moveSprite(mySprite, 150, 0)
-    info.startCountdown(300)
-    for (let index = 0; index < 500; index++) {
+    for (let index = 0; index < 50; index++) {
         pause(200)
         top.setPosition(randint(0, 155), 1)
         projectile = sprites.createProjectileFromSprite(img`
@@ -446,11 +489,55 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
+            `, top, randint(-10, 10), 150)
+    }
+    pause(50)
+    for (let index = 0; index < 20000000; index++) {
+        pause(50)
+        top.setPosition(randint(0, 155), 1)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, top, randint(-20, 20), 200)
+    }
+    for (let index = 0; index < 20000000; index++) {
+        pause(25)
+        top.setPosition(randint(0, 155), 1)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
             `, top, randint(-30, 30), 100)
     }
-    pause(1000)
-    for (let index = 0; index < 2000; index++) {
-        pause(50)
+    for (let index = 0; index < 20000000; index++) {
+        pause(15)
         top.setPosition(randint(0, 155), 1)
         projectile = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
@@ -472,21 +559,19 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             `, top, randint(-40, 40), 100)
     }
     game.gameOver(true)
-    game.setGameOverEffect(true, effects.confetti)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
+    music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     pause(1000)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.liv, function (sprite2, otherSprite2) {
     info.changeLifeBy(1)
     sprites.destroy(liv2, effects.hearts, 1000)
-})
-info.onCountdownEnd(function () {
-	
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 1000000; index++) {
         pause(randint(1000, 5000))
         mySprite2 = sprites.create(img`
             . . . . . . . . . . . . . . . . 
@@ -534,59 +619,88 @@ controller.A.onEvent(ControllerButtonEvent.Released, function () {
     liv2.setPosition(randint(0, 150), 0)
     liv2.setVelocity(0, 100)
 })
-controller.B.onEvent(ControllerButtonEvent.Released, function () {
-    for (let index = 0; index < 10; index++) {
-        pause(randint(1000, 5000))
-        mySprite2 = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . 9 9 9 9 9 9 9 . . . . . 
-            . . . . 9 9 9 9 9 9 9 . . . . . 
-            . . . . 9 9 8 8 8 9 9 . . . . . 
-            . . . . 9 9 8 5 8 9 9 . . . . . 
-            . . . . 9 9 8 8 8 9 9 . . . . . 
-            . . . . 9 9 9 9 9 9 9 . . . . . 
-            . . . . 9 9 9 9 9 9 9 . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Food)
-        mySprite2.setPosition(randint(0, 150), 0)
-        mySprite2.setVelocity(0, 100)
+info.onLifeZero(function () {
+    let top_scores = settings.readNumberArray("leaderboard")
+if (top_scores == null) {
+        top_scores = []
     }
-    liv2 = sprites.create(img`
-        ....................
-        ....................
-        ....................
-        ....2222...2222.....
-        ...222222.222222....
-        ..222212222222222...
-        ..222122222222222...
-        ..221222222222222...
-        ..222222222222222...
-        ..222222222222222...
-        ..222222222222222...
-        ...2222222222222....
-        ....22222222222.....
-        .....222222222......
-        ......2222222.......
-        .......22222........
-        ........222.........
-        .........2..........
-        .........2..........
-        ....................
-        `, SpriteKind.liv)
-    liv2.setPosition(randint(0, 150), 0)
-    liv2.setVelocity(0, 100)
+    // 2. Tilføj din nuværende score
+    current_score = info.score()
+    top_scores.push(current_score)
+    top_scores.sort()
+top_scores.reverse()
+    // 4. Behold kun de 5 bedste
+    if (top_scores.length > 5) {
+        top_scores = top_scores.slice(0, 5)
+    }
+    settings.writeNumberArray("leaderboard", top_scores)
+// 6. Lav teksten manuelt med en løkke (meget mere stabilt)
+    leaderboard_text = "TOP 5 SCORES:"
+    for (let score_val of top_scores) {
+        leaderboard_text = "" + leaderboard_text + "\n" + ("" + score_val)
+    }
+    // 7. Vis og slut
+    game.showLongText(leaderboard_text, DialogLayout.Center)
+    game.gameOver(false)
+})
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    for (let index = 0; index < 100000; index++) {
+        for (let index = 0; index < 10; index++) {
+            pause(randint(1000, 5000))
+            mySprite2 = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . 9 9 9 9 9 9 9 . . . . . 
+                . . . . 9 9 9 9 9 9 9 . . . . . 
+                . . . . 9 9 8 8 8 9 9 . . . . . 
+                . . . . 9 9 8 5 8 9 9 . . . . . 
+                . . . . 9 9 8 8 8 9 9 . . . . . 
+                . . . . 9 9 9 9 9 9 9 . . . . . 
+                . . . . 9 9 9 9 9 9 9 . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Food)
+            mySprite2.setPosition(randint(0, 150), 0)
+            mySprite2.setVelocity(0, 100)
+        }
+        liv2 = sprites.create(img`
+            ....................
+            ....................
+            ....................
+            ....2222...2222.....
+            ...222222.222222....
+            ..222212222222222...
+            ..222122222222222...
+            ..221222222222222...
+            ..222222222222222...
+            ..222222222222222...
+            ..222222222222222...
+            ...2222222222222....
+            ....22222222222.....
+            .....222222222......
+            ......2222222.......
+            .......22222........
+            ........222.........
+            .........2..........
+            .........2..........
+            ....................
+            `, SpriteKind.liv)
+        liv2.setPosition(randint(0, 150), 0)
+        liv2.setVelocity(0, 100)
+    }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite3, otherSprite3) {
     info.changeScoreBy(100)
     sprites.destroy(mySprite2, effects.confetti, 500)
+    music.play(music.melodyPlayable(music.magicWand), music.PlaybackMode.UntilDone)
 })
+let leaderboard_text = ""
+let current_score = 0
 let mySprite2: Sprite = null
 let liv2: Sprite = null
 let projectile: Sprite = null
@@ -612,3 +726,5 @@ mySprite3 = sprites.create(img`
     c c c c c c c c c c c c c c . . 
     `, SpriteKind.wizard)
 mySprite3.sayText("press a to easy press b to hard", 200000, false)
+// Sæt volumen lidt ned i starten, så det ikke brager
+music.setVolume(80)
